78fc5b128d5ac87803de668004fa7c1ed4846c72
masterUNG <phrombutr@gmail.com>
2016-11-11 13:36:15 +0700

    Read JSON Finish

---
+++ app/src/main/java/ubu/chantharo/nanthiya/easyshop/MyAdapter.java
@@ -0,0 +1,73 @@
+package ubu.chantharo.nanthiya.easyshop;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by masterUNG on 11/11/2016 AD.
+ */
+
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context context;
+    private String[] nameStrings, detailStrings, phoneStrings, iconStrings;
+    private TextView nameTextView, detailTextView, phoneTextView;
+    private ImageView imageView;
+
+    public MyAdapter(Context context,
+                     String[] nameStrings,
+                     String[] detailStrings,
+                     String[] phoneStrings,
+                     String[] iconStrings) {
+        this.context = context;
+        this.nameStrings = nameStrings;
+        this.detailStrings = detailStrings;
+        this.phoneStrings = phoneStrings;
+        this.iconStrings = iconStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return nameStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view1 = layoutInflater.inflate(R.layout.my_layout, viewGroup, false);
+
+        //Bind Widget
+        nameTextView = (TextView) view1.findViewById(R.id.textView5);
+        detailTextView = (TextView) view1.findViewById(R.id.textView6);
+        phoneTextView = (TextView) view1.findViewById(R.id.textView7);
+        imageView = (ImageView) view1.findViewById(R.id.imvIcon);
+
+        //Show View
+        nameTextView.setText(nameStrings[i]);
+        detailTextView.setText(detailStrings[i]);
+        phoneTextView.setText(phoneStrings[i]);
+
+        Picasso.with(context).load(iconStrings[i]).into(imageView);
+
+        return view1;
+    }
+}   // Main Class
